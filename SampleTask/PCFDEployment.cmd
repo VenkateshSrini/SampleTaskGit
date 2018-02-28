2cbebb48880bff7b1ff72f089795342d6f934cb9
VenkateshSrinivasan <venkateshsrini3@gmail.com>
2018-02-28 08:41:49 +0530

    Base commit

---
+++ SampleTask/PCFDEployment.cmd
@@ -0,0 +1,4 @@
+﻿REM Deployment in PCF
+cf push -u none
+cf stop SampleTask
+cf run-task SampleTask "dotnet SampleTask.dll "Sample"" --name execute-sample-task
---
+++ SampleTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SampleTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] != "null")
+                Console.WriteLine($"Hello {args[0]}!");
+            else
+                Console.WriteLine("Testing null start");
+            
+            Console.Read();
+        }
+    }
+}
